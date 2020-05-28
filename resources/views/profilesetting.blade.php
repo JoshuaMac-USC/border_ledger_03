e348bfed0f88610caad2addadd89974c23ae035f
JoshuaMac-USC <macaldojoshua23@gmail.com>
2020-05-28 18:40:42 +0800

    Update profilesetting.blade.php

--- resources/views/profilesetting.blade.php
+++ resources/views/profilesetting.blade.php
@@ -42,7 +42,7 @@
 </center>
 <!-- Modal -->
 <div class="modal fade" id="EditUser" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Edit Profile</h5>
